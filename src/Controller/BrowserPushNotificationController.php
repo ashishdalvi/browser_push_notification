e40a150f050e57597ef6e8fdc5ccac55b75b34a7
shylaja <shylaja.r@iksula.com>
2017-05-04 17:13:55 +0530

    latest files

--- src/Controller/BrowserPushNotificationController.php
+++ src/Controller/BrowserPushNotificationController.php
@@ -4,10 +4,10 @@ namespace Drupal\browser_push_notification\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Drupal\Core\Database\Connection;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\browser_push_notification\Model\SubscriptionsDatastorage;
-use Drupal\browser_push_notification\Model\NotificationsDatastorage;
 
 /**
  * Controller routines for Browser push notification.
@@ -30,44 +30,27 @@ class BrowserPushNotificationController extends ControllerBase {
    */
   public function __construct(Connection $database) {
     $this->database = $database;
-  }
 
-  /**
-   * This event will be triggered when browser got a push notification.
-   */
-  public function getNotificationRoute() {
-
-    $notificationData = NotificationsDatastorage::loadAll();
-    if (!empty($notificationData)) {
-      foreach ($notificationData as $key => $notification) {
-        $data['notification']['title'] = $notification->title;
-        $data['notification']['body'] = $notification->body;
-        $data['notification']['icon'] = $notification->icon;
-        $data['notification']['url'] = $notification->url;
-      }
-    }
-    return new JsonResponse($data);
   }
 
   /**
    * This event will be triggered when user subscribe for notification.
    */
-  
-  public function subscribe() {
-    $data['Authorization'] = \Drupal::request()->request->get('authorization');
-    $data['cryptokey'] = \Drupal::request()->request->get('cryptokey');
-    $entry['subscription_endpoint'] = \Drupal::request()->request->get('endpoint');
-    $auth_string = $data['Authorization'];
-    $entry['subscription_data'] = serialize(array('auth' => $auth_string, 'crypto_key' => $data['cryptokey']));
-    $entry['registered_on'] = strtotime(date('Y-m-d H:i:s'));
-    $result = SubscriptionsDatastorage::insert($entry);
-    return new JsonResponse($notification_send);
+  public function subscribe(Request $request) {
+    if ($request) {
+      $data = json_decode($request->getContent(), TRUE);
+      $notification_send = NULL;
+      $entry['subscription_endpoint'] = $data['endpoint'];
+      $entry['subscription_data'] = serialize(['key' => $data['key'], 'token' => $data['token']]);
+      $entry['registered_on'] = strtotime(date('Y-m-d H:i:s'));
+      $success = SubscriptionsDatastorage::insert($entry);
+      return new JsonResponse([$success]);
+    }
   }
 
-   /**
+  /**
    * List of all subscribed users.
    */
-  
   public function subscriptionList() {
     // The table description.
     $header = [
@@ -88,7 +71,7 @@ class BrowserPushNotificationController extends ControllerBase {
     $query->fields(SubscriptionsDatastorage::$browserSubscriptionTable, $getFields);
     // Limit the rows to 50 for each page.
     $pager = $query->extend('Drupal\Core\Database\Query\PagerSelectExtender')
-        ->limit(SubscriptionsDatastorage::$browserSubscriptionCount);
+      ->limit(SubscriptionsDatastorage::$browserSubscriptionCount);
     $result = $pager->execute();
 
     // Populate the rows.
@@ -98,16 +81,18 @@ class BrowserPushNotificationController extends ControllerBase {
         'data' => [
           'id' => $row->id,
           'register_id' => $row->subscription_endpoint,
-          'date' => date('d/m/Y', $row->registered_on)
+          'date' => date('d/m/Y', $row->registered_on),
         ],
       ];
     }
-    if (empty($rows))
-      $markup = 'No record found.';
-    else
-      $markup = 'List of All Subscribed Users.';
+    if (empty($rows)) {
+      $markup = $this->t('No record found.');
+    }
+    else {
+      $markup = $this->t('List of All Subscribed Users.');
+    }
     $build = [
-      '#markup' => $this->t($markup),
+      '#markup' => $markup,
     ];
     // Generate the table.
     $build['config_table'] = [
--- src/Form/PushNotificationForm.php
+++ src/Form/PushNotificationForm.php
@@ -7,7 +7,6 @@ use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Database\Connection;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\browser_push_notification\Model\SubscriptionsDatastorage;
-use Drupal\browser_push_notification\Model\NotificationsDatastorage;
 
 /**
  * Class PushNotificationForm.
@@ -49,7 +48,7 @@ class PushNotificationForm extends FormBase {
     $form['sendMessage'] = [
       '#type' => 'fieldset',
       '#title' => $this->t('Browser Notification Details'),
-     ];
+    ];
     $form['sendMessage']['title'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Notification Title'),
@@ -57,12 +56,12 @@ class PushNotificationForm extends FormBase {
       '#size' => 100,
       '#description' => $this->t('Enter the Title of the Notification.'),
     ];
-    
+
     $form['sendMessage']['body'] = [
       '#type' => 'textarea',
       '#required' => TRUE,
       '#title' => $this->t('Notification Message'),
-      '#maxlength' => 150,
+      '#maxlength' => 300,
       '#description' => $this->t('Enter the Message of the Notification.'),
     ];
 
@@ -79,12 +78,12 @@ class PushNotificationForm extends FormBase {
       '#title' => $this->t('Notification URL'),
       '#description' => $this->t('Enter the URL on which user will redirect after clicking on Notification.Eg.http://example.com/test-contents'),
     ];
-    
-    $form['sendMessage']['submit'] = array(
+
+    $form['sendMessage']['submit'] = [
       '#type' => 'submit',
-      '#value' => t('Send Notification'),
-    );
-       
+      '#value' => $this->t('Send Notification'),
+    ];
+
     return $form;
   }
 
@@ -92,41 +91,53 @@ class PushNotificationForm extends FormBase {
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
-    return TRUE;
+    if (!(filter_var($form_state->getValue('url'), FILTER_VALIDATE_URL))) {
+      $form_state->setErrorByName('url', t('Not a valid url.'));
+    }
+    if (!(filter_var($form_state->getValue('icon'), FILTER_VALIDATE_URL))) {
+      $form_state->setErrorByName('icon', t('Not a valid image url.'));
+    }
   }
 
   /**
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    // Store the notification values in browser_notification table to trigger notification
     $account = $this->currentUser();
-    // Save the submitted entry.
-    $entry = array(
+    $entry = [
       'title' => $form_state->getValue('title'),
       'body' => $form_state->getValue('body'),
       'icon' => $form_state->getValue('icon'),
       'url' => $form_state->getValue('url'),
       'created_date' => strtotime(date('Y-m-d H:i:s')),
       'uid' => $account->id(),
-    );
-    $return = NotificationsDatastorage::insert($entry);
+    ];
+    $notification_data = $entry['title'] . '<br>';
+    $notification_data .= $entry['body'] . '<br>';
+    $notification_data .= $entry['icon'] . '<br>';
+    $notification_data .= $entry['url'] . '<br>';
     $subscriptions = SubscriptionsDatastorage::loadAll();
-    if (!empty($subscriptions)) {
+    $bpn_public_key = \Drupal::config('browser_push_notification.settings')->get('bpn_public_key');
+    $bpn_private_key = \Drupal::config('browser_push_notification.settings')->get('bpn_private_key');
+    if (empty($bpn_public_key) && empty($bpn_private_key)) {
+      drupal_set_message($this->t('Please set public & private key.'), 'error');
+    }
+    if (!empty($subscriptions) && !empty($bpn_public_key) && !empty($bpn_private_key)) {
       $batch = [
         'title' => $this->t('Sending Push Notification...'),
         'operations' => [
           [
             '\Drupal\browser_push_notification\Model\SubscriptionsDatastorage::sendNotificationStart',
-            [$subscriptions],
+            [$subscriptions, $notification_data],
           ],
         ],
-        'finished' => '\Drupal\browser_push_notification\Model\SubscriptionsDatastorage::sendNotificationFinished',
+        'finished' => '\Drupal\browser_push_notification\Model\SubscriptionsDatastorage::notificationFinished',
       ];
       batch_set($batch);
+      drupal_set_message($this->t('Push notification sent successfully to  @entry users', ['@entry' => print_r(count($subscriptions), TRUE)]));
     }
-    if ($return) {
-       drupal_set_message(t('Push notification sent successfully to  @entry users', array('@entry' => print_r(count($subscriptions), TRUE))));
+    else {
+      drupal_set_message($this->t('Subscription list is empty.'), 'error');
     }
   }
 
